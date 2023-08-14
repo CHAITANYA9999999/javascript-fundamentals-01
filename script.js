27e522124460455adfd8c0ce04b9b643078793b5
CHAITANYA9999999 <chiknum@gmail.com>
2023-08-14 18:03:26 +0530

    fundamentals part 1

---
+++ script.js
@@ -0,0 +1,15 @@
+// let js = 'amazing';
+// console.log(typeof Null == "undefined");
+
+// const marks_w=78;
+// const marks_h=1.69;
+// const john_w=92;
+// const john_h=1.95;
+// const marks_bmi = marks_w/(marks_h**2);
+// const john_bmi = john_w/(john_h**2);
+// markHigherBMI = marks_bmi > john_bmi;
+// alert("mark has higher bmi: "+markHigherBMI);
+
+// const a = `I'm Marks, my weight is ${marks_w} and my height is ${marks_h}`;
+// console.log(a);
+// alert(a);
